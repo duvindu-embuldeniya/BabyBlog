dd76fe7296c66c77b332e1c22ef16531e55fd76f
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-02-19 19:57:09 +0530

    +data

--- baby_blog/baby_blog/settings.py
+++ baby_blog/baby_blog/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER =os.environ.get('user')
-EMAIL_HOST_PASSWORD =os.environ.get('app_pass')
+EMAIL_HOST_USER =os.environ.get('email')
+EMAIL_HOST_PASSWORD =os.environ.get('app_password')
